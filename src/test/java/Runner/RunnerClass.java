29a4ced8cbcd399dc4bbe8d84d0d70aa944366cc
sruqaiyaa@gmail.com <ruqaiyaa.ruqaiyaa@capgemini.com>
2024-04-03 10:01:45 +0530

    first commit

--- src/test/java/Runner/RunnerClass.java
+++ src/test/java/Runner/RunnerClass.java
@@ -1,15 +1,9 @@
 package Runner;
-import org.junit.runner.RunWith;
-
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-//import io.cucumber.testng.CucumberOptions;
-import io.cucumber.junit.Cucumber;
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "Features" ,
-glue = "Stepdefinition",
-monochrome=true)
 
-public class RunnerClass {
+@CucumberOptions(features = "Features" ,
+glue = "Stepdefinition")
+public class RunnerClass extends AbstractTestNGCucumberTests{
 
 }
